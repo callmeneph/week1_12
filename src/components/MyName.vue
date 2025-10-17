1bdef99fca9865adc61a7a4aaa941b70006e17dd
CallMeNephilim <mahdiafkar0@gmail.com>
2025-10-17 13:43:30 +1100

    showing our name in the web page

---
+++ src/components/MyName.vue
@@ -0,0 +1,11 @@
+<template>
+  <p>Hello, my name is <strong>Mahdi Afkar</strong>!</p>
+</template>
+
+<script>
+export default { name: 'MyName' }
+</script>
+
+<style scoped>
+p { color: teal; font-size: 1.2rem; }
+</style>
